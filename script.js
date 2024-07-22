b4d141e37b6b72e1e1d9d35ce5dc488463f6159e
Hossam.AlBadry <hossam.albadry@hypercell.net>
2024-07-23 02:03:04 +0300

    finalize edits and await for feedback

--- script.js
+++ script.js
@@ -1,94 +1,152 @@
 let students = {};
+//we will define 2 mappers for 1st prep and 2nd prep
+const expectedColumns = {
+  sheet1: {
+    // 0: "م"
+    student_number: 0,
+    // 1: "كود الطالب"
+    student_code: 1,
+    // 2: "اسم التلميذ"
+    student_name: 2,
+    // 3: "رقم الجلوس"
+    seat_number: 3,
+    // 4: "الجنسية"
+    student_nationality: 4,
+    // 5: "الديانة"
+    student_regelion: 5,
+    // 6: "حالة القيد"
+    student_status: 6,
+    // 7: "الرقم السري"
+    student_password: 7,
+    // 8: "موقف الدمج"
+    merging_position: 8,
+    // 9: "اللجنة"
+    section: 9,
+    // 10: "اللغة الثانية"
+    second_language: 10,
+    // 11: "الفصل"
+    class: 11,
+    // 12: "النوع"
+    student_gender: 12,
+    // 13: "دفع المصروفات"
+    payment_of_expences: 13,
+    // 14: "المادة"
+    subject: 14,
+    // 15: "اللغــــــــــة العربيــــــــــة و الخط"
+    arabic: 23,
+    // 25: "اللغــــــــــة الأجنبية"
+    english: 33,
+    // 35: "الدراســــــــــات الاجتماعيــــــــــة"
+    social: 43,
+    // 45: "الرياضيـــــــــــــــــــــــــــــــات"
+    math: 55,
+    // 57: "العلـــــــــــــــــــــــــــــوم"
+    science: 67,
+    // 69: "المجموع الأساسي"
+    main_total_mark: 69,
+    // // 71: "نشاط تربوي1 "
+    // // 77: "نشاط تربوي2 "
+    // // 83: "المجموع الكلي بالأنشطة"
+    // 85: "التربيــــــــــة الفنيــــــــــة"
+    additional_tf: 93,
+    // 95: "الحاســـــــــــب الآلــــــــــــي"
+    additional_computer: 105,
+    // 107: "التربية الرياضية"
+    additional_sport: 111,
+    // 113: "التربيــــــــــة الدينيــــــــــة"
+    additional_regelion: 121,
+    // 123: "المشروع البحثي"
+    additional_reseach_project: 123,
+    // 128: "اللغة الأولي مستوي رفيع"
+    additional_english_AL: 136,
+    // 138: "اللغة الثانية مستوي رفيع"
+    additional_second_language: 146,
+    // // 148: "إظهار أو حجب النتيجة"
+    show_or_hide_result: 148,
+    // // 149: "ملاحظات"
+    notes: 149,
+    // 150: "نتيجة آخر العام"
+    last_year_result: 150,
+  },
+  sheet2: {
+    // 0: "م"
+    student_number: 0,
+    // 1: "كود الطالب"
+    student_code: 1,
+    // 2: "اسم التلميذ"
+    student_name: 2,
+    // 3: "رقم الجلوس"
+    seat_number: 3,
+    // 4: "حالة القيد"
+    student_status: 4,
+    // 5: "اللغة الثانية"
+    second_language: 5,
+    // 6: "الفصل"
+    class: 6,
+    // 7: "اللغــــــــــة العربيــــــــــة و الخط"
+    arabic: 15,
+    // 17: "اللغــــــــــة الأجنبية"
+    english: 25,
+    // 27: "الدراســــــــــات الاجتماعيــــــــــة"
+    social: 35,
+    // 37: "الرياضيـــــــــــــــــــــــــــــــات"
+    math: 47,
+    // 49: "العلـــــــــــــــــــــــــــــوم"
+    science: 59,
+    // 61: "المجموع الأساسي"
+    main_total_mark: 61,
+    // 63: "نشاط تربوي1 "
+    // 69: "نشاط تربوي2 "
+    // 75: "المجموع الكلي بالأنشطة"
+    // 77: "التربيــــــــــة الفنيــــــــــة"
+    additional_tf: 85,
+    // 87: "الحاســـــــــــب الآلــــــــــــي"
+    additional_computer: 97,
+    // 99: "التربية الرياضية"
+    additional_sport: 103,
+    // 105: "التربيــــــــــة الدينيــــــــــة"
+    additional_regelion: 113,
+    // 115: "المشروع البحثي"
+    additional_reseach_project: 118,
+    // 120: "اللغة الأولي مستوي رفيع"
+    additional_english_AL: 128,
+    // 130: "اللغة الثانية مستوي رفيع"
+    additional_second_language: 138,
+    // 140: "إظهار أو حجب النتيجة"
+    show_or_hide_result: 140,
+    // 141: "ملاحظات"
+    notes: 141,
+    // 142: "نتيجة آخر العام"
+    last_year_result: 142,
+  },
+};
 
 function loadExcel() {
-  fetch("شيت لحسن 1 ع 2024.xls")
+  fetch("شيت لحسام 1 ، 2 ع 2024.xls")
     .then((res) => res.arrayBuffer())
     .then((data) => {
       const workbook = XLSX.read(data, { type: "array" });
-      const sheet = workbook.Sheets[workbook.SheetNames[0]];
-      const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
 
-      // console.log(jsonData);
-      // Assuming the first row is the table header
-      const headers = jsonData[0];
-      // console.log(headers);
-      for (let i = 1; i < jsonData.length; i++) {
-        const row = jsonData[i];
-        const studentCode = row[1]; // Assuming second column is the student code
-        const studentSeatNumber = row[3];
-        const studentData = {
-          // 0: "م"
-          student_number: row[0],
-          // 1: "كود الطالب"
-          student_code: row[1],
-          // 2: "اسم التلميذ"
-          student_name: row[2],
-          // 3: "رقم الجلوس"
-          seat_number: row[3],
-          // 4: "الجنسية"
-          student_nationality: row[4],
-          // 5: "الديانة"
-          student_regelion: row[5],
-          // 6: "حالة القيد"
-          student_status: row[6],
-          // 7: "الرقم السري"
-          student_password: row[7],
-          // 8: "موقف الدمج"
-          merging_position: row[8],
-          // 9: "اللجنة"
-          section: row[9],
-          // 10: "اللغة الثانية"
-          second_language: row[10],
-          // 11: "الفصل"
-          class: row[11],
-          // 12: "النوع"
-          student_gender: row[12],
-          // 13: "دفع المصروفات"
-          payment_of_expences: row[13],
-          // 14: "المادة"
-          subject: row[14],
-          // 15: "اللغــــــــــة العربيــــــــــة و الخط"
-          arabic: row[23],
-          // 25: "اللغــــــــــة الأجنبية"
-          english: row[33],
-          // 35: "الدراســــــــــات الاجتماعيــــــــــة"
-          social: row[43],
-          // 45: "الرياضيـــــــــــــــــــــــــــــــات"
-          math: row[55],
-          // 57: "العلـــــــــــــــــــــــــــــوم"
-          science: row[67],
-          // 69: "المجموع الأساسي"
-          main_total_mark: row[69],
-          // // 71: "نشاط تربوي1 "
-          // additional_tf_1: row[75],
-          // // 77: "نشاط تربوي2 "
-          // additional_tf_2: row[81],
-          // // 83: "المجموع الكلي بالأنشطة"
-          // additional_total: row[83],
-          // 85: "التربيــــــــــة الفنيــــــــــة"
-          additional_tf: row[93],
-          // 95: "الحاســـــــــــب الآلــــــــــــي"
-          additional_computer: row[105],
-          // 107: "التربية الرياضية"
-          additional_sport: row[111],
-          // 113: "التربيــــــــــة الدينيــــــــــة"
-          additional_regelion: row[121],
-          // // 123: "المشروع البحثي"
-          // additional_reseach_project: row[123],
-          // 128: "اللغة الأولي مستوي رفيع"
-          additional_english_AL: row[136],
-          // 138: "اللغة الثانية مستوي رفيع"
-          additional_second_language: row[146],
-          // // 148: "إظهار أو حجب النتيجة"
-          // show_or_hide_result: row[148],
-          // // 149: "ملاحظات"
-          // notes: row[149],
-          // 150: "نتيجة آخر العام"
-          last_year_result: row[150],
-        };
-        students[studentSeatNumber] = studentData;
-        students[studentCode] = studentData;
-      }
+      workbook.SheetNames.forEach((sheetName, index) => {
+        const sheet = workbook.Sheets[sheetName];
+        const jsonData = XLSX.utils.sheet_to_json(sheet, { header: 1 });
+        const columnMapping =
+          index === 0 ? expectedColumns.sheet1 : expectedColumns.sheet2;
+        console.log(jsonData);
+        for (let i = 1; i < jsonData.length; i++) {
+          const row = jsonData[i];
+          const studentCode = row[columnMapping.student_code]; // Assuming second column is the student code
+          const studentSeatNumber = row[columnMapping.seat_number];
+          const studentData = {};
+
+          for (const [key, columnIndex] of Object.entries(columnMapping)) {
+            studentData[key] = row[columnIndex] !== undefined ? row[columnIndex] : "N/A";
+          }
+
+          students[studentSeatNumber] = studentData;
+          students[studentCode] = studentData;
+        }
+      });
     })
     .catch((err) => console.error(err));
 }
@@ -104,7 +162,6 @@ function search() {
   }
 
   if (student) {
-    // console.log(student);
     let student_name = document.getElementById("student_name");
     let arabic_mark = document.getElementById("arabic_mark");
     let english_mark = document.getElementById("english_mark");
@@ -122,16 +179,17 @@ function search() {
     let additional_regelion_mark = document.getElementById(
       "additional_regelion_mark"
     );
+
+    let additional_reseach_project = document.getElementById(
+      "additional_reseach_project"
+    );
     let additional_english_AL_mark = document.getElementById(
       "additional_english_AL_mark"
     );
     let additional_second_language_AL_mark = document.getElementById(
       "additional_second_language_AL_mark"
     );
-    let last_year_result_mark = document.getElementById(
-      "last_year_result_mark"
-    );
-
+    
     student_name.innerText = student.student_name || "N/A";
     arabic_mark.innerText = Math.round(student.arabic) || "N/A";
     english_mark.innerText = Math.round(student.english) || "N/A";
@@ -140,26 +198,42 @@ function search() {
     science_mark.innerText = Math.round(student.science) || "N/A";
     main_total_mark.innerText = Math.round(student.main_total_mark) || "N/A";
     additional_tf_mark.innerText = Math.round(student.additional_tf) || "N/A";
-    additional_computer_mark.innerText =
-      Math.round(student.additional_computer) || "N/A";
-    additional_sport_mark.innerText =
-      Math.round(student.additional_sport) || "N/A";
-    additional_regelion_mark.innerText =
-      Math.round(student.additional_regelion) || "N/A";
-    additional_english_AL_mark.innerText =
-      Math.round(student.additional_english_AL) || "N/A";
-    additional_second_language_AL_mark.innerText =
-      Math.round(student.additional_second_language) || "N/A";
-    last_year_result_mark.innerText = student.last_year_result || "N/A";
+    additional_computer_mark.innerText = Math.round(student.additional_computer) || "N/A";
+    additional_sport_mark.innerText = Math.round(student.additional_sport) || "N/A";
+    additional_regelion_mark.innerText = Math.round(student.additional_regelion) || "N/A";
+    additional_reseach_project.innerHTML = Math.round(student.additional_reseach_project) || "N/A";
+    additional_english_AL_mark.innerText = Math.round(student.additional_english_AL) || "N/A";
+    additional_second_language_AL_mark.innerText = Math.round(student.additional_second_language) || "N/A";
+    
+    let last_year_result_mark = document.getElementById("last_year_result_mark");
+    let show_or_hide_result = document.getElementById("show_or_hide_result");
+    let table = document.getElementById("table");
+    let table_container = document.getElementById("table-container");
+    console.log(student.show_or_hide_result);
+    /*first we make a condition if show result is true */
+    let show = student.show_or_hide_result == "N/A" ? true : false;
+    if (show) {
+      /*show the table with it's style and hide rejection text*/
+      show_or_hide_result.style.display = "none";
+      table.style.display = "block";
+      //this was work on web but it break on mobile observe it hasan right?
+      table_container.style.display = "flex";
+      table_container.style.justifyContent = "center";
+      table_container.style.alignContent = "center";
+      last_year_result_mark.innerHTML = student.last_year_result || "N/A";
+    } else {
+      /*hide the table with it's style and show rejection text*/
+      show_or_hide_result.style.display = "block";
+      show_or_hide_result.style.justifyContent = "center";
+      show_or_hide_result.style.alignContent = "center";
+      show_or_hide_result.innerHTML = student.show_or_hide_result;
+      table.style.display = "none";
+      last_year_result_mark.innerHTML = "";
 
-    document.getElementById("table").style.display = "block";
-    //this was work on web but it break on mobile observe it hasan right?
-    document.getElementsByClassName("table-container")[0].style.display = "flex";
-    document.getElementsByClassName("table-container")[0].style.justifyContent = "center";
-    document.getElementsByClassName("table-container")[0].style.alignContent = "center";
+    }
   } else {
     alert("Student not found with input : " + input);
-    document.getElementById("table").style.display = "none";
+    table.style.display = "none";
   }
 }
 
